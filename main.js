3d7551cb182b73a492ff89a9cab426f6ab785816
phatphoom <99587354+phatphoom@users.noreply.github.com>
2022-06-30 06:33:55 +0700

    rebuild

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("new");
+var cards = document.querySelectorAll('.card');
+
+[...cards].forEach((card)=>{
+  card.addEventListener( 'click', function() {
+    card.classList.toggle('is-flipped');
+  });
+});
